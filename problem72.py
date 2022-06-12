adc2862dd70804b7f5f91548a8c2cfb4e59b0999
Timothe <timothe.wt@gmail.com>
2022-06-12 02:40:05 +0200

    solved

--- problem72.py
+++ problem72.py
@@ -1,55 +1,53 @@
-"""import sys
-
-sys.setrecursionlimit(3000000)
-
-
-### Stern-Brocot Tree
-
-
-class Node:
-    def __init__(self, val, l_son, r_son, l_neighbour, r_neighbour):
-        self.val = val  # [num, den]
-        self.l_son = l_son
-        self.r_son = r_son
-        self.l_neighbour = l_neighbour  # [num, den]
-        self.r_neighbour = r_neighbour  # [num, den]
-
-    def count_nodes(self):
-        if self.l_son is None or self.r_son is None:
-            return 1
-        else:
-            return 1 + self.l_son.count_nodes() + self.r_son.count_nodes()
-
-    def display(self):
-        print(self.val)
-        if self.l_son is not None:
-            self.l_son.display()
-        if self.r_son is not None:
-            self.r_son.display()
-
-
-def build_tree(val, l_neighbour, r_neighbour):
-    if val[1] > 1_000_000:
-        return None
+### Using a Stern-Brocot tree
+# However this solution is not optimal and would take hours even days to compute
+"""def count_nodes_recursive(limit, l_d, r_d):
+    den = l_d + r_d
+    if den > limit:
+        return 0
     else:
-        l_s = build_tree([l_neighbour[0] + val[0], l_neighbour[1] + val[1]], l_neighbour, val)
-        r_s = build_tree([val[0] + r_neighbour[0], val[1] + r_neighbour[1]], val, r_neighbour)
-        return Node(val, l_s, r_s, l_neighbour, r_neighbour)
-# Recursive method is too long and my computer doesn't go to the end
+        return 1 + count_nodes_recursive(limit, l_d, den) + count_nodes_recursive(limit, den, r_d)
 
 
-tree = build_tree([1, 2], [0, 1], [1, 1])
-print(tree.count_nodes())"""
+def count_nodes_iteraive(limit):
+    count = 0
+    left = 1
+    right = 1
+    stack = []
+    while True:
+        med = left + right
+        if med > limit:
+            if len(stack) == 0:
+                break
+            else:
+                left = right
+                right = stack.pop()
+        else:
+            count += 1
+            stack.append(right)
+            right = med
+    return count
+
+res = count_nodes_iteraive(12000)
+
+print("Result : ", res)"""
+
+# using Euler's totient formula (executes in few seconds) :
+# phi(n) = n * product of (1 - 1 / p) with p being all the distinct prime numbers that divide n
 
 N = 1_000_000
 phi = []
-for i in range(0, N + 1):
+
+for i in range(0, N + 1):  # we generate the list of |[0; N]|
     phi.append(i)
-for i in range(2, N + 1):
-    if phi[i] == i:
-        for j in range(i, N + 1, i):
-            phi[j] -= phi[j] // i
 
-print(sum(phi) - 1)
+for k in range(2, N + 1):
+    if phi[k] == k:  # if the index is equal to the current number it is prime, otherwise it would have been
+        # multiplied by a prime number below him
+        for m in range(k, N + 1, k):    # we multiply every multiple of this prime number p by 1-1/p as in the formula
+            phi[m] -= phi[m] // k
+# we then have a list representing the result of the Euler's totient for every integer in |[0; N]|
+# an interesting fact is that every number whose index is him + 1 is a prime number
+# (because phi(p) = p-p/p = p-1, he's the only prime that can divide him)
 
-# not my solution
+res = sum(phi) - 1  # -1 because 1 is in the list but doesn't count in this case
+print("Number of reduced proper fraction with a denominator below or equal to", N, ":", res)
