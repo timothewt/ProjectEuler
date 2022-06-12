8956cd19b9ecac0530656cbc0ac58fd956c66ed8
Timothe <timothe.wt@gmail.com>
2022-06-12 20:29:29 +0200

    solved 92!

--- problem92.py
+++ problem92.py
@@ -4,22 +4,26 @@ def next_node(n):
         res += int(i) ** 2
     return res
 
+
 def chain_until_stuck(n):
     start = n
     while n != 89 and n != 1:
         n = next_node(n)
         if n in known_nodes:
             n = ending[known_nodes.index(n)]
+            return n
     known_nodes.append(start)
     ending.append(n)
     return n
+
 
 known_nodes = []
 ending = []
 
 res = 0
-for i in range(1, 10 ** 5):
+
+for i in range(1, 10_000_000):
     if chain_until_stuck(i) == 89:
         res += 1
 
-print(res)
+print(res, "numbers below 10,000,000 will at the end of the chain arrive at 89.")
